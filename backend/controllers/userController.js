d54e13490dd8c0bea03ced134850f50f2edffdc7
ThaiSonDev <sonthaidev91@gmail.com>
2023-05-07 09:43:32 +0700

    Generate Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -31,6 +36,9 @@ const registerUser = asyncHandler(async (req, res) => {
   // const user = await User.create({ name, email, password: hashedPassword });
   const user = await User.create({ name, email, password });
 
+  //Generate Token
+  const token = generateToken(user._id);
+
   if (user) {
     const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
@@ -40,6 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
       photo,
       phone,
       bio,
+      token,
     });
   } else {
     res.status(400);
