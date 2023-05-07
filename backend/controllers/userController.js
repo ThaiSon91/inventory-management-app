9e27b3d75554e7138f21d82f8c0c3c40bea084a4
ThaiSonDev <sonthaidev91@gmail.com>
2023-05-07 07:34:37 +0700

    Encrypt password user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -22,8 +23,12 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Email has alreadly been registered");
   }
 
+  //Encrypt password before saving to DB
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   //Create new user
-  const user = await User.create({ name, email, password });
+  const user = await User.create({ name, email, password: hashedPassword });
   if (user) {
     const { _id, name, email, photo, phone, bio } = user;
     res.status(201).json({
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,7 +20,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please add a Password"],
       minLength: [6, "Password must be up to 6 characters"],
-      maxLength: [23, "Password must not be more than 23 characters"],
+      // maxLength: [23, "Password must not be more than 23 characters"],
     },
     photo: {
       type: String,
